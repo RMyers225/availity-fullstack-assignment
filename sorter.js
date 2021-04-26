17503241795296883dd2b0c1bb7e51a0ff0bd0f7
RMyers225 <62179690+RMyers225@users.noreply.github.com>
2021-04-26 14:26:57 -0400

    sorter

---
+++ sorter.js
@@ -0,0 +1,10 @@
+var insuranceCompanies = ["Blue Cross", "United Healthcare", "Aetna", "Hene Health", "Cigna"];
+
+var lastName = ["Myers,David", "Smith,Jennifer", "Matthews,Eric", "Adams,Franklin", "Hart,Tori"];
+
+insuranceCompanies.sort()
+lastName.sort()
+
+console.log(insuranceCompanies)
+console.log(lastName)
+
